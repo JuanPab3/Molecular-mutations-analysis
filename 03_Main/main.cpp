3ff6eb7428c58325997b5d21967681b96a750840
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-04-26 15:47:41 -0500

    Smith–Waterman Algorithm Implementation
    issue no: UD012

--- 03_Main/main.cpp
+++ 03_Main/main.cpp
@@ -8,12 +8,17 @@ using namespace std;
 
 int main() {
 
+    // string a = "TGTTACGG";
+    // string b = "GGTTGACTA";
+
     string a = "ACCGTGA";
     string b = "GTGAATA";
 
-    ScoringMatrix mat(a,b);
 
+    ScoringMatrix mat(a,b);
     mat.show();
+    mat.show_value_box();
+    mat.show_string_box();
 
     return 0;
 }
--- 03_Main/swa.hpp
+++ 03_Main/swa.hpp
@@ -1,5 +1,5 @@
-#ifndef _SWA_
-#define  _SWA_
+#ifndef _SW_A_ /*Smith–Waterman Algorithm*/
+#define  _SW_A_
 
 #include <iostream>
 #include <string>
@@ -8,55 +8,54 @@
 
 class ScoringMatrix {
 private:
+    /*Class Structs*/
+    struct ValBox { /*Contenedor para los valores importantes en matrix*/
+        int ival;
+        int jval;
+    };
 
-    int penE = -1,  /*Penalización por Cuadro Vacio Estandar*/
-        penD = -1,  /*Penalización por Desigualdad*/
-        ganI =  1;  /*Ganancia por Igualdad */
+    ValBox create_val_box(int i, int j);
 
-    int M;  /*Cantidad de Filas*/
+    struct AlignedCode {
+        std::string codeA = "";
+        std::string codeB = "";
+    };
+
+    /*Private Containers*/
+    std::stack<ValBox> ValueBox;
+    std::stack<AlignedCode> StringBox;
+
+
+    int max  =  0;  /*Es el numero con el valor más alto dentro de toda la tabla*/
+    int penE = -2,  /*Penalización por Cuadro Vacio Estandar*/
+        penD = -3,  /*Penalización por Desigualdad*/
+        ganI =  3;  /*Ganancia por Igualdad */
+
+    std::string codeString1;
+    std::string codeString2;
+    int **matrix;
     int N;  /*Cantidad de Columnas*/
+    int M;  /*Cantidad de Filas*/
 
     /*Private Methods*/
-    void maxVal (int** matrix, int i, int j, char iChar, char jChar); /*O(st.size())*/
-    int intMax(std::stack<int> & st); /*O(st.size())*/
+    void max_val (int** matrix, int i, int j, char iChar, char jChar); /*O(st.size())*/
+    int int_max(std::stack<int> & st); /*O(st.size())*/
     int s (char & a,char & b); /*O(1)*/
 
+    void valBox_to_string(ScoringMatrix::AlignedCode & alig,  int i, int j);
+    void string_to_box(std::stack<ValBox> ValueBox);
+
 public:
-    int **matrix;
     /*Constructors&Destructors*/
     ScoringMatrix(std::string cd1, std::string cd2); /*O(M*N)*/
     ~ScoringMatrix(); /*O(N)*/
 
-    /*Accessor Methods*/
-    void show() const; /*O(M*N)*/
+    /*Accessor Methods*/ /*Estas funciones estan para comprobar los resultados*/
+    void show_value_box () const;
+    void show_string_box () const;
+    void show () const; /*O(M*N)*/ /*Representa a ScoringMatrix como una matriz*/
 
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif /*_SWA_*/
+#endif /*_SW_A_*/
